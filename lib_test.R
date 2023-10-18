1164e747cc56e8d574ff8a815c73f81d94069375
Brendon Phillips <brendoncphillips@gmail.com>
2023-10-18 19:23:30 -0400

    offline spot-testing for the library

--- lib_test.R
+++ lib_test.R
@@ -4,22 +4,25 @@ setwd("C:/Users/brendon phillips/Documents/GitHub/rothlab_permtest/")
 
 library(devtools)
 
-detach("package:SepsisTools", unload = TRUE)
-devtools::install_github("brendonphillips/SepsisTools", ref="main", force=TRUE)
+# detach("package:SepsisTools", unload = TRUE)
+# devtools::install_github("brendonphillips/SepsisTools", ref="main", force=TRUE)
+
+devtools::document(); devtools::load_all()
 
 library(SepsisTools)
 library(data.table)
 
-haha <- fread("C:/Users/brendon phillips/Documents/GitHub/Roth-Lab-Github-Repository/permutation_test_code/data.csv")
+data("class_performance")
 
-perm <- global_permutation_test(
-    haha,
-    group_name = "group",
-    id_name = "part_id",
-    event_name = "event",
-    systematic = TRUE,
-    ntrials = 10000,
-    parallel = TRUE,
-    ranseed = 20000
-)
+# perm <- global_permutation_test(
+#     haha,
+#     group_name = "group",
+#     id_name = "part_id",
+#     event_name = "event",
+#     systematic = TRUE,
+#     ntrials = 10000,
+#     parallel = TRUE,
+#     ranseed = 20000
+# )
 
+devtools::document(); devtools::load_all()
