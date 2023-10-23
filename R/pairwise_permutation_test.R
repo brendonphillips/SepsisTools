d56417f3808558866472380d2d2732bd392d28f2
Brendon Phillips <brendoncphillips@gmail.com>
2023-10-23 15:59:24 -0400

    code finished
    1. step-down procedure argument changed from string to vector
    2. pairwise tests done (by splitting the data table at the outset
    3. final results collated (list -> table conversions)
    4. family corrections added

--- R/pairwise_permutation_test.R
+++ R/pairwise_permutation_test.R
@@ -25,15 +25,48 @@ pairwise_permutation_tests <- function(data_,
                                        na_fill = FALSE,
                                        backsolve = FALSE,
                                        reference_group = "placebo",
-                                       step_down_procedure = "HB",
+                                       step_down_procedures = c("BH"),
                                        global_test_first = TRUE,
-                                       verbose = FALSE) {
+                                       verbose = TRUE) {
     
     if (global_test_first) {
         
-        # if global test is done first, always send the message to the user
-        gt_results <- global_permutation_test(
-            data_,
+        # # if global test is done first, always send the message to the user
+        # gt_results <- global_permutation_test(
+        #     data_,
+        #     group_col_name = group_col_name,
+        #     id_col_name = id_col_name,
+        #     event_col_name = event_col_name,
+        #     ntrials = ntrials,
+        #     parallel = parallel,
+        #     ranseed = ranseed,
+        #     systematic = systematic,
+        #     na_fill = na_fill,
+        #     verbose = TRUE
+        # )
+    }
+    
+    
+    data_groups <- unique(data_[[group_col_name]])
+    if (! reference_group %in% data_groups) {
+        stop(sprintf(
+            "Requested reference group `%s` is not among the groups in the %s data frame. Aborting pairwise permutation test.", 
+            reference_group, deparse(substitute(data_))
+        ))
+    }
+    other_groups <- data_groups %>% .[. != reference_group] %>% unique
+    
+    pairwise_test_results <- list()
+    
+    for (comparing_to in other_groups) {
+        
+        pairwise_test_results[[comparing_to]] <- global_permutation_test(
+            # janky code to do the group filtering based on the unique name given
+            # will change 
+            data_ %>%
+                rename(temp_group = !!group_col_name) %>%
+                subset(temp_group %in% c(comparing_to, reference_group)) %>%
+                rename(!!group_col_name := !!"temp_group"),
             group_col_name = group_col_name,
             id_col_name = id_col_name,
             event_col_name = event_col_name,
@@ -42,26 +75,39 @@ pairwise_permutation_tests <- function(data_,
             ranseed = ranseed,
             systematic = systematic,
             na_fill = na_fill,
-            verbose = TRUE
+            verbose = verbose
         )
     }
     
-    data_groups <- unique(data_[[group_col_name]])
+    final_res <- pairwise_test_results %>%
+        map(as_tibble) %>%
+        reduce(bind_rows) %>%
+        mutate(
+            group_name = other_groups,
+            reference_group = reference_group
+        ) # %>%
+        # relocate(sym(group_name), N, p, error)
     
-    if (! reference_group %in% data_groups) {
-        stop(sprintf(
-            "Requested reference group `%s` is not among the groups in the %s data frame. Aborting pairwise permutation test.", 
-            reference_group, deparse(substitute(data_))
-        ))
+
+    family_corrections <- step_down_procedures %>%
+        .[. %in% p.adjust.methods & . != "none"]
+
+    if (length(family_corrections) > 0) {
+
+        final_res <- sapply(
+                unique(step_down_procedures),
+                \(x) p.adjust(final_res$p, eval(x))
+            ) %>%
+            as.data.frame %>%
+            tibble %>%
+            rename_with(~paste0("p_adj_", .x)) %>%
+            cbind(final_res)
     }
-    
-    pairwise_test_results <- list()
-    
-    for(other_group in data_groups[data_groups != reference_group]) {
-        
-        next   
-    }
-    
-    return(0)
-    
+
+    return(final_res)
+
 }
+
+# of only 2 groups, change message to paireiser test
+# if only a single group, thenthrow error
+# column names in lower case to avoid error
