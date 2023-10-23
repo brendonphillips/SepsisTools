3acdc9f69730bcae8734e94826a14faccbbfb579
Brendon Phillips <brendoncphillips@gmail.com>
2023-10-22 21:20:01 -0400

    changed parametrisation, message
    1. "verbose" parameter included for printing global test results to the screen,
    2. default arguments changed to make function non-obtrusive,
    3. completion message moved to the global_perm* function

--- R/pairwise_permutation_test.R
+++ R/pairwise_permutation_test.R
@@ -15,9 +15,9 @@
 #' @export
 pairwise_permutation_tests <- function(data_,
                                        ...,
-                                       group_name = "group",
-                                       id_name = "id",
-                                       event_name = "event",
+                                       group_col_name = "group",
+                                       id_col_name = "id",
+                                       event_col_name = "event",
                                        ntrials = 100,
                                        parallel = FALSE,
                                        ranseed = NaN,
@@ -26,67 +26,42 @@ pairwise_permutation_tests <- function(data_,
                                        backsolve = FALSE,
                                        reference_group = "placebo",
                                        step_down_procedure = "HB",
-                                       global_test_first = TRUE) {
+                                       global_test_first = TRUE,
+                                       verbose = FALSE) {
     
     if (global_test_first) {
         
+        # if global test is done first, always send the message to the user
         gt_results <- global_permutation_test(
             data_,
-            group_name = group_name,
-            id_name = id_name,
-            event_name = event_name,
+            group_col_name = group_col_name,
+            id_col_name = id_col_name,
+            event_col_name = event_col_name,
             ntrials = ntrials,
             parallel = parallel,
             ranseed = ranseed,
             systematic = systematic,
-            na_fill = na_fill
+            na_fill = na_fill,
+            verbose = TRUE
         )
-        
-        data_groups <- unique(data_[[group_name]])
-        
-        message_text <- paste0(
-            "\nPreliminary Global Test:", 
-            sprintf("\n\nNumber of obervations : %s.", nrow(data_)),
-            sprintf("\n\nGroups (N=%s): [%s].", 
-                    length(data_groups),
-                    paste(data_groups, collapse=", ")
-            ),
-            "\n\n",
-            data_ %>% 
-                rename(group_ := group_name) %>%
-                group_by(group_) %>% 
-                summarise(N = n()) %>%
-                mutate(text_ = sprintf(
-                    "\tGroup %s (%s): %s observations,", 
-                    1:n(), group_, N)
-                ) %>% 
-                pull(text_) %>% 
-                paste0(collapse="\n"),
-            ".",
-            sprintf(
-                paste0(
-                    "\n\nGlobal Test:",
-                    "\n\n\tNumber of test statistics calculated: %s.",
-                    "\n\tp-value = %s, Monte-Carlo error = %s."
-                ),
-                gt_results$N, gt_results$p, round(gt_results$error, 6)
-            ),
-            "\n\n"
-        )
-        
-        message(message_text)
-        
-        if (! reference_group %in% data_groups) {
-            stop(sprintf(
-                "Requested reference group `%s` is not among the groups in the %s data frame. Aborting pairwise permutation test.", 
-                reference_group, deparse(substitute(data_))
-            ))
-        }
-        
-        for(other_group in )
-        
     }
-
+    
+    data_groups <- unique(data_[[group_col_name]])
+    
+    if (! reference_group %in% data_groups) {
+        stop(sprintf(
+            "Requested reference group `%s` is not among the groups in the %s data frame. Aborting pairwise permutation test.", 
+            reference_group, deparse(substitute(data_))
+        ))
+    }
+    
+    pairwise_test_results <- list()
+    
+    for(other_group in data_groups[data_groups != reference_group]) {
+        
+        next   
+    }
+    
     return(0)
     
 }
