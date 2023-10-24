5561804cd817b6dcdc202be6a6aa5a5010434c38
Brendon Phillips <brendoncphillips@gmail.com>
2023-10-23 22:14:41 -0400

    testing

--- testbench.R
+++ testbench.R
@@ -3,6 +3,7 @@ rm(list = ls())
 setwd("C:/Users/brendon phillips/Documents/GitHub/rothlab_permtest/")
 
 library(devtools)
+library(roxygen2)
 library(dplyr)
 
 # detach("package:SepsisTools", unload = TRUE)
@@ -12,22 +13,44 @@ library(dplyr)
 # 
 library(SepsisTools)
 
+roxygen2::update_collate(".")
 devtools::document()
 devtools::load_all()
 
 data("class_performance")
 
-# perm <- global_permutation_test(
-#     haha,
-#     group_col_name = "group",
-#     id_col_name = "part_id",
-#     event_col_name = "event",
-#     systematic = TRUE,
-#     ntrials = 10000,
-#     parallel = TRUE,
-#     ranseed = 20000
-# )
-# 
+
+group_col_name <- "class_teacher"
+id_col_name <- "student_id"
+event_col_name <- "passed_exam_num"
+na_fill <- TRUE
+
+
+# haha <- permgp_fn(class_performance,
+#                   group_col_name = group_col_name,
+#                   id_col_name = id_col_name,
+#                   event_col_name = event_col_name,
+#                   systematic = TRUE,
+#                   na_fill = TRUE,
+#                   ranseed = 5)
+# print(haha)
+
+# haha <- get_p_value(class_performance,
+#                     group_col_name = group_col_name,
+#                     id_col_name = id_col_name,
+#                     event_col_name = event_col_name,
+#                     systematic = TRUE,
+#                     na_fill = TRUE,
+#                     ranseed = 5)
+# print(haha)
+
+# hehe <- class_performance %>%
+#     standard_table(id_col_name = id_col_name,
+#                    group_col_name = group_col_name,
+#                    event_col_name = event_col_name,
+#                    na_fill = TRUE)
+# print(hehe)
+
 # temp <- class_performance %>%
 #     select(student_id, class_teacher, passed_exam_num) %>%
 #     rename(
@@ -40,110 +63,43 @@ data("class_performance")
 #         group_ = as.numeric(factor(group_)),
 #         event_ = as.numeric(factor(event_))
 #     )
-#
-temp <- tibble(
-        group_ = rep(letters[1:3], 2),
-        event_ = c(rep(0,3), rep(1,3))
-    ) %>%
-    mutate(id_ = 1:n()) %>%
-    add_row(group_ = "c", event_ = 0, id_ = 3) %>%
-    add_row(group_ = "a", event_ = 1, id_ = 1) %>%
-    relocate(id_,) %>%
-    arrange(id_)
-# 
-# 
+
+# temp <- tibble(
+#         group_ = rep(letters[1:3], 2),
+#         event_ = c(rep(0,3), rep(1,3))
+#     ) %>%
+#     mutate(id_ = 1:n()) %>%
+#     add_row(group_ = "c", event_ = 0, id_ = 3) %>%
+#     add_row(group_ = "a", event_ = 1, id_ = 1) %>%
+#     relocate(id_,) %>%
+#     arrange(id_)
+
 # # haha <- class_performance %>% 
 # haha <- temp %>%
-#     permute_groups_(systematic = TRUE)
-#     
-#     
-# # haha_filtered <- class_performance  %>%
-# haha_filtered <- temp %>%
-#     permute_groups_(systematic = TRUE) %>%  
-#     subset(
-#         group_ %in% letters[1:2] &  
-#         perm_group_ %in% letters[1:2]
-#     )
-#     
-# haha_specific <- temp %>%
-#     subset(group_ %in% letters[1:2])
-# 
-# haha_purpose <- haha_specific %>%
-#     subset(!is.na(group_))  %>%
-#     permute_groups_(systematic = TRUE)
-#     
-# writeLines("\n\n")
-# 
-# print("haha_specific")
-# print(haha_specific)
-# 
-# print("haha_filtered")
-# print(haha_filtered)
-# 
-# print("haha_purpose")
-# print(haha_purpose)
-# 
-# print(setdiff(haha_filtered, haha_purpose))
-# 
-# print(setdiff(haha_purpose, haha_filtered))
+#     permute_groups(systematic = TRUE)
 
-
-
-
-
-# hehe <- class_performance %>% 
-#     select(all_of(c(id_col_name, group_col_name, event_col_name))) %>%
-#     mutate(.sep.entry.id = 1:n()) %>%
-#     rename(
-#         id_ = !!id_col_name,
-#         group_ = !!group_col_name,
-#         event_ = !!event_col_name
-#     ) %>%
-#     mutate(
-#         group_ = case_when(
-#             (na_fill) & is.na(group_) ~ ".NA_group",
-#             TRUE ~ group_
-#         )
-#     )
-
-haha <- get_p_value(class_performance,
-    group_col_name = group_col_name,
-    id_col_name = id_col_name,
-    event_col_name = event_col_name,
-    systematic = TRUE,
-    na_fill = TRUE)
-
-# haha <- permgp_fn(class_performance,
-#     group_col_name = group_col_name,
-#     id_col_name = id_col_name,
-#     event_col_name = event_col_name,
-#     systematic = TRUE,
-#     na_fill = TRUE)
-# 
 # haha <- global_permutation_test(class_performance,
 #                     group_col_name = group_col_name,
 #                     id_col_name = id_col_name,
 #                     event_col_name = event_col_name,
-#                     ntrials = 100,
+#                     ntrials = 1000,
 #                     parallel = TRUE,
 #                     ranseed = NaN,
 #                     systematic = TRUE,
-#                     na_fill = FALSE,
+#                     na_fill = TRUE,
 #                     verbose = TRUE)
 
-# haha <- pairwise_permutation_tests(class_performance,
-#                                    group_col_name = group_col_name,
-#                                    id_col_name = id_col_name,
-#                                    event_col_name = event_col_name,
-#                                    ntrials = 100,
-#                                    reference_group = "Roth",
-#                                    parallel = FALSE,
-#                                    global_test_first = TRUE,
-#                                    verbose = FALSE,
-#                                    step_down_prodecures = c("BH"),
-#                                    na_fill = TRUE)
-# troubleshoot parallel processing problems here
-# print(haha)
+haha <- pairwise_permutation_tests(class_performance,
+                                   group_col_name = group_col_name,
+                                   id_col_name = id_col_name,
+                                   event_col_name = event_col_name,
+                                   ntrials = 100,
+                                   reference_group = "Roth",
+                                   parallel = FALSE,
+                                   global_test_first = TRUE,
+                                   verbose = FALSE,
+                                   step_down_prodecures = c("BH"),
+                                   na_fill = TRUE)
 
 
 
@@ -170,30 +126,11 @@ haha <- get_p_value(class_performance,
 #     )
     
 
-
-
-group_col_name <- "class_teacher"
-id_col_name <- "student_id"
-event_col_name <- "passed_exam_num"
-na_fill <- TRUE
-
-
-standardised_table <- class_performance %>%
-    select(all_of(c(id_col_name, group_col_name, event_col_name))) %>%
-    mutate(.sep.entry.id = 1:n()) %>%
-    rename(
-        id_ = !!id_col_name,
-        group_ = !!group_col_name,
-        event_ = !!event_col_name
-    ) %>%
-    mutate(
-        group_ = case_when(
-            (na_fill) & is.na(group_) ~ ".NA_group",
-            TRUE ~ group_
-        )
-    ) %>%
-    subset(!is.na(group_))
-
+# 
+# haha <- standard_table(class_performance,
+#                        group_col_name = group_col_name,id_col_name = id_col_name,
+#                        event_col_name = event_col_name,
+#                        na_fill = FALSE)
 
 
 
