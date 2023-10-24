ca18eb9af0e4a7a134f91a6d239da454f3381f4e
Brendon Phillips <brendoncphillips@gmail.com>
2023-10-23 22:17:05 -0400

    removed message, changed to account for standard table

--- R/global_permutation_test.R
+++ R/global_permutation_test.R
@@ -26,9 +26,9 @@
 #' @export
 global_permutation_test <- function(data_,
                                     ...,
-                                    group_col_name = "group",
-                                    id_col_name = "id",
-                                    event_col_name = "event",
+                                    group_col_name = "group_",
+                                    id_col_name = "id_",
+                                    event_col_name = "event_",
                                     ntrials = 10000,
                                     parallel = FALSE,
                                     ranseed = NaN,
@@ -37,22 +37,22 @@ global_permutation_test <- function(data_,
                                     check_ = FALSE,
                                     verbose = FALSE) {
     
+    standard_data <- standard_table(data_,
+                                    group_col_name = group_col_name,
+                                    id_col_name = id_col_name,
+                                    event_col_name = event_col_name,
+                                    na_fill = na_fill)
+    
     # warning("Note that the output of this function has changed. it is now a list giving $p (the p-value), $stddev (the standard deviation) $error (the Monte-Carlo error) and $N (the number of test statistics calculated). Please see documentation.")
-    
-    if (!is.na(ranseed) && is.numeric(ranseed)) {
-        set.seed(ranseed)
-    } else {
-        set.seed(Sys.time())
-    }
-    
+
     teststat <- perm_test_statistic(
-        data_[[event_col_name]],
-        data_[[group_col_name]]
+        standard_data$event_,
+        standard_data$group_
     )
     
-    num_trials <- min(ntrials, factorial(nrow(data_)))
+    num_trials <- min(ntrials, factorial(nrow(standard_data)))
     
-    data_groups <- unique(data_[[group_col_name]])
+    data_groups <- unique(standard_data$group_)
     
     permutation_test_type <- ifelse(
         length(data_groups) > 2,
@@ -65,38 +65,35 @@ global_permutation_test <- function(data_,
     )
     
     if (parallel) {
-        
+
         pool_size <- max(detectCores() - 2, 1)
-        
+
         message(sprintf(
             "\nExecuting %s Permutation Test (%s) in parallel: %s workers",
             permutation_test_type, competing_groups, pool_size
         ))
-        
+
         perm_cluster <- makeCluster(pool_size)
         registerDoSNOW(perm_cluster)
-        
+
         prog_bar <- txtProgressBar(max = num_trials, style = 3)
         progress <- function(n) setTxtProgressBar(prog_bar, n)
         opts <- list(progress = progress)
-        
+
         teststat_null <- tryCatch({
-            
+
             foreach(1:num_trials,
                     .combine = "c",
                     .export=c("get_p_value"),
                     .packages = c("dplyr"),
                     .options.snow = opts) %dopar% {
-                        
-                        get_p_value(data_,
-                                    group_col_name = group_col_name,
-                                    id_col_name = id_col_name,
-                                    event_col_name = event_col_name,
+
+                        get_p_value(standard_data,
+                                    ranseed = ranseed,
                                     systematic = systamatic,
-                                    na_fill = na_fill,
                                     verbose = verbose)
-                        
-                        
+
+
                     }
         },
         error = function(e) {
@@ -107,71 +104,64 @@ global_permutation_test <- function(data_,
             message(sprintf("\nWarning() encountered during parallel execution: %s", w))
         }
         )
-        
+
         message("\n")
         stopCluster(perm_cluster)
-        
+
     } else {
-        
+
         message(sprintf(
             "\n%s Permutation Test (%s): serial execution selected",
             permutation_test_type, competing_groups
         ))
-        
-        
+
         teststat_null <- array(data = NaN, dim = num_trials)
-        
+
         prog_bar = txtProgressBar(max = num_trials, initial = 0, style = 3)
-        
+
         for (idx in 1:num_trials) {
             setTxtProgressBar(prog_bar, idx)
-            teststat_null[idx] <- get_p_value(data_,
-                                           group_col_name = group_col_name,
-                                           id_col_name = id_col_name,
-                                           event_col_name = event_col_name,
+            teststat_null[idx] <- get_p_value(standard_data,
                                            ranseed = ranseed,
                                            systematic = systematic,
-                                           na_fill = na_fill,
                                            verbose = verbose)
         }
-        
+
         message("\n")
     }
     
     if (check_) {
-        # check whether the members of teststat_null are different from each 
-        # other; can go wrong when we don;t use the perm_group_ column when 
-        # calculating the test statistic. make an upper triangular matrix out of 
-        # the values and calculate the determinant. test whether the determinant 
-        # is consistently e-close to the nth power of randomly chosen element of 
-        # the vector. if so, the elements on the vector are all the same 
+        # check whether the members of teststat_null are different from each
+        # other; can go wrong when we don;t use the perm_group_ column when
+        # calculating the test statistic. make an upper triangular matrix out of
+        # the values and calculate the determinant. test whether the determinant
+        # is consistently e-close to the nth power of randomly chosen element of
+        # the vector. if so, the elements on the vector are all the same
     }
     
     p_value <- mean(teststat_null >= teststat, na.rm = TRUE)
     mc_error <- sqrt(p_value*(1-p_value)/num_trials)
-    num_res <- length(teststat_null)
-    
-    data_groups <- unique(data_[[group_col_name]])
-    
+    num_stats <- length(teststat_null)
+    num_obs <- nrow(standard_data)
+
     if (verbose) {
-        
+
         message_text <- paste0(
             sprintf("\n%s Permutation Test:", permutation_test_type),
-            sprintf("\n\nTotal number of obervations : %s.", nrow(data_)),
-            sprintf("\n\nGroups (N=%s): [%s].", 
+            sprintf("\n\nTotal number of obervations : %s.", num_obs),
+            sprintf("\n\nGroups (N=%s): [%s].",
                     length(data_groups),
                     paste(data_groups, collapse=", ")
             ),
             "\n\n",
-            data_ %>% 
-                rename(group_ := group_col_name) %>%
-                group_by(group_) %>% 
+            standard_data %>%
+                group_by(group_) %>%
                 summarise(N = n()) %>%
                 mutate(text_ = sprintf(
-                    "\tGroup %s (%s): %s observations,", 
+                    "\tGroup %s (%s): %s observations,",
                     1:n(), group_, N)
-                ) %>% 
-                pull(text_) %>% 
+                ) %>%
+                pull(text_) %>%
                 paste0(collapse="\n"),
             ".",
             sprintf(
@@ -180,19 +170,20 @@ global_permutation_test <- function(data_,
                     "\n\tNumber of test statistics calculated: %s.",
                     "\n\tp-value = %s, Monte-Carlo error = %s."
                 ),
-                num_res, p_value, round(mc_error, 6)
+                num_stats, p_value, round(mc_error, 6)
             ),
             "\n\n***************"
         )
-        
+
         message(message_text)
-        
+
     }
-    
+
     return(list(
-        p = p_value, 
-        error = mc_error, 
-        N = num_res
+        p = p_value,
+        error = mc_error,
+        N_trials = num_stats,
+        N_obs = num_obs
     ))
     
 }
